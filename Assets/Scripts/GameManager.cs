5a2f041e2714f9a81f644d79953209ed87106942
kailermai <kailermaioff@gmail.com>
2024-05-17 01:28:30 +0800

    added GameManager object and script
    added functions and singleton, linked unityevent and function for onEnemyDestroyed

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public int health;
+    public int money;
+
+    [Header("Components")]
+    public TextMeshProUGUI healthAndMoneyText;
+
+    [Header("Events")]
+    public UnityEvent onEnemyDestroyed;
+    public UnityEvent onMoneyChanged;
+
+    //Singleton
+    public static GameManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void UpdateHealthAndMoneyText()
+    {
+        healthAndMoneyText.text = $"Health: {health}\nMoney: ${money}";
+    }
+
+    public void AddMoney(int amount)
+    {
+        money += amount;
+
+        UpdateHealthAndMoneyText();
+        onMoneyChanged.Invoke();
+    }
+
+    public void TakeMoney(int amount)
+    {
+        money -= amount;
+
+        UpdateHealthAndMoneyText();
+        onMoneyChanged.Invoke();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+
+        UpdateHealthAndMoneyText();
+
+        if (health <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+
+    }
+
+    void WinGame()
+    {
+
+    }
+
+    public void OnEnemyDestroyed()
+    {
+
+    }
+}
